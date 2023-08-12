108c17005598da5996bb04c573e54a8a3de28a68
Lucas Blante <lucasviniciusblante@gmail.com>
2023-08-12 17:51:23 -0300

    FEAT: refactoring design and js logic

--- scripts.js
+++ scripts.js
@@ -1,63 +1,58 @@
-const inputPhone = document.querySelector("#phone");
-const inputText = document.querySelector("#text");
-const inputSubmit = document.querySelector("#submit");
-const link = document.querySelector("#link");
+const inputPhone = document.querySelector('#phone')
+const inputText = document.querySelector('#text')
+const inputSubmit = document.querySelector('#submit')
+const link = document.querySelector('#link')
 
 inputPhone.addEventListener(`focus`, function () {
-  inputPhone.style = "border-bottom: 3px solid #25d366";
-  if (inputPhone.value == "") {
-    inputPhone.value = "+55";
-  }
-});
+  inputPhone.style = 'border-bottom: 3px solid #16826b'
+})
 
-inputPhone.addEventListener("focusout", function () {
-  inputPhone.style = "border-bottom: 3px solid #fff";
-  if (inputPhone.value == "+55") {
-    inputPhone.value = "";
-  } else if (inputPhone.value.length > 0 && inputPhone.value.length < 13) {
-    alert("Verifique o número de telefone digitado.");
-    inputPhone.style = "border-bottom: 3px solid #e53d30";
-  }
-});
+inputPhone.addEventListener('focusout', function () {
+  inputPhone.style = 'border-bottom: 3px solid #fff'
+})
 
-inputText.addEventListener("focus", function () {
-  inputText.style = "border-bottom: 3px solid #25d366";
-});
+inputText.addEventListener('focus', function () {
+  inputText.style = 'border-bottom: 3px solid #16826b'
+})
 
-inputText.addEventListener("focusout", function () {
-  inputText.style = "border-bottom: 3px solid #fff";
-});
+inputText.addEventListener('focusout', function () {
+  inputText.style = 'border-bottom: 3px solid #fff'
+})
 
-inputSubmit.addEventListener("click", function () {
-  var text;
+inputSubmit.addEventListener('click', function () {
+  handleSubmit()
+})
 
-  if (inputPhone.value != "") {
-    if (inputText.value != "") {
-      text = inputText.value
-        .replaceAll(" ", "+")
-        .replaceAll("?", "%3F")
-        .replaceAll("!", "%21");
+function handleSubmit() {
+  var phone = inputPhone.value
+  var message = inputText.value
+
+  if (phone.length > 10) {
+    phone = phone.replace(/\D/g, '')
+
+    if (message) {
+      message = encodeURIComponent(message)
     } else {
-      const saudacao = getSaudacao();
-      text = `${saudacao}+%C3%89+da+THS+Inform%C3%A1tica%21+Tudo+bem%3F`;
+      message = getSaudacao()
     }
-    link.target = "_blank";
-    link.href = `https://api.whatsapp.com/send?phone=${inputPhone.value}&text=${text}`;
+
+    link.target = '_blank'
+    link.href = `https://wa.me/send?phone=${phone}&text=${message}`
   } else {
-    alert("Verifique o número de telefone digitado.");
-    inputPhone.style = "border-bottom: 3px solid #e53d30";
+    alert('Verifique o número de telefone digitado.')
+    inputPhone.style = 'border-bottom: 3px solid #e53d30'
   }
-});
+}
 
 function getSaudacao() {
-  const date = new Date();
-  const hour = date.getHours();
+  const date = new Date()
+  const hour = date.getHours()
 
   if (hour >= 5 && hour <= 11) {
-    return "Bom dia!";
+    return encodeURIComponent('Bom dia! É da THS Informática, tudo bem?')
   } else if (hour >= 12 && hour <= 17) {
-    return "Boa Tarde";
+    return encodeURIComponent('Boa tarde! É da THS Informática, tudo bem?')
   } else {
-    return "Boa Noite!";
+    return encodeURIComponent('Boa noite! É da THS Informática, tudo bem?')
   }
 }
